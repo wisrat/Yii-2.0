ed68101d98544729949b4b2a7cc8833a7521a3fb
wisrat <wisart@o2.pl>
2017-08-25 18:26:35 +0200

    Test
    test

---
+++ config/db.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=yii_base',
+    'username' => 'root',
+    'password' => 'root',
+    'charset' => 'utf8',
+];
